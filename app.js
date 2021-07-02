d693657b4ef38b8ef41c084e4bd7eeee17e8746e
Joseph-K-B <303josephb@gmail.com>
2021-07-01 17:55:21 -0700

    added draw button

--- app.js
+++ app.js
@@ -6,15 +6,16 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
-import { userWin } from "./utils.js";
+import { userWin } from './utils.js';
 
 const playBtn = document.getElementById('play-game');
 const winSpan = document.getElementById('total-wins');
 const lossSpan = document.getElementById('total-losses');
+const drawSpan = document.getElementById('total-draws');
 console.log(playBtn);
 let wins = 0;
 let losses = 0;
-
+let draws = 0;
 playBtn.addEventListener('click', ()=>{
     console.log('clicked');
     const pic = document.querySelector('input[type=radio]:checked');
@@ -41,7 +42,11 @@ playBtn.addEventListener('click', ()=>{
     else if (isWinner === 'lose') {
         losses++;
     }
+    else {
+        draws++;
+    }
     winSpan.textContent = wins;
     lossSpan.textContent = losses;
+    drawSpan.textContent = draws;
 });
 
